f10046f63e5e5e039a36b80108c1b2a70cdd4c96
Anti-code <Anti-code@users.noreply.github.com>
2016-03-15 14:11:37 +0200

    Threaded sniffer returns OrderedDict

--- sniff.py
+++ sniff.py
@@ -1,11 +1,14 @@
 from scapy.all import *
 from collections import OrderedDict
-from threading import Thread
+from threading import Thread, Event
 import time
 class PacketSniffer():
     def __init__(self, interface = 'wlan0'):
         self.packet = None
-        Thread(target = sniff, kwargs = {"iface" : interface, "store" : 0, "prn" : lambda packet: self.packetToDict(packet)}, daemon = True).start()
+        self.interface = interface
+        self._stop = Event()
+        self.t = Thread(target = sniff,  kwargs = {"iface" : self.interface, "store" : 0, "prn" : lambda packet: self.packetToDict(packet)}, daemon = True)
+        /*
     def packetToDict(self,data):
         packet = OrderedDict()
         packet["real"] = data
@@ -16,5 +19,7 @@ class PacketSniffer():
             packet[data.name] = list(data.fields.values())
             data = data.payload
         self.packet = packet
+    def run(self):
+        self.t.start()
 if __name__ == "__main__":
     ps = PacketSniffer()
