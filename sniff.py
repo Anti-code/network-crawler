4ce67334fe76773ac4cd81983f889acffc02bb90
Anti-code <Anti-code@users.noreply.github.com>
2016-03-28 17:00:33 +0300

    Now fetches and parses each packet properly

--- sniff.py
+++ sniff.py
@@ -2,24 +2,74 @@ from scapy.all import *
 from collections import OrderedDict
 from threading import Thread, Event
 import time
-class PacketSniffer():
+class PacketSniffer(object):
     def __init__(self, interface = 'wlan0'):
-        self.packet = None
         self.interface = interface
-        self._stop = Event()
-        self.t = Thread(target = sniff,  kwargs = {"iface" : self.interface, "store" : 0, "prn" : lambda packet: self.packetToDict(packet)}, daemon = True)
-        /*
-    def packetToDict(self,data):
-        packet = OrderedDict()
-        packet["real"] = data
-        while data:
-            if type(data) is NoPayload:
+        self.packet = None
+        self.cookie = False
+
+    def fetch(self, packet):
+        data = OrderedDict()
+        data = {"no":"-",
+            "time":"-",
+            "ips":"-",
+            "macs":packet.src,
+            "ports":"-",
+            "ipd":"-",
+            "macd":packet.dst, 
+            "portd":"-",
+            "sum" : packet.summary()}
+        while packet:
+            if type(packet) is NoPayload:
                 break
-            packet["time"]=int(time.time())
-            packet[data.name] = list(data.fields.values())
-            data = data.payload
-        self.packet = packet
+            elif type(packet) is IP:
+                data["ips"] = packet.src
+                data["ipd"] = packet.dst
+            elif type(packet) is TCP:
+                data["ports"] = packet.sport
+                data["portd"] = packet.dport
+            elif type(packet) is UDP:
+                data["ports"] = packet.sport
+                data["portd"] = packet.dport
+            packet = packet.payload
+        return data
+
     def run(self):
-        self.t.start()
+        if self.cookie:                                  #2
+            yield self.fetch(sniff(filter= "Ether", iface=self.interface, store = 1, count=1)[-1])
+        
+def hammerOn(ps):
+     while ps.cookie:
+            packet = ps.run().__next__()
+            data = {"no":"",
+            "time":"",
+            "ips":"-",
+            "macs":packet.src,
+            "ports":"-",
+            "ipd":"-",
+            "macd":packet.dst, 
+            "portd":"-",
+            "sum" : packet.summary()}
+            while packet:
+                if type(packet) is NoPayload:
+                    break
+                elif type(packet) is IP:
+                    data["ips"] = packet.src
+                    data["ipd"] = packet.dst
+                elif type(packet) is TCP:
+                    data["ports"] = packet.sport
+                    data["portd"] = packet.dport
+                elif type(packet) is UDP:
+                    data["ports"] = packet.sport
+                    data["portd"] = packet.dport
+                packet = packet.payload
+           
 if __name__ == "__main__":
     ps = PacketSniffer()
+    ps.cookie = True
+    while ps.cookie:
+        print(ps.run().__next__())
+    #hammerOn(ps)
+
+   
+    #ps.run()
