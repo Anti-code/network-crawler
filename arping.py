a04dbebcf17ab0d499463f6380ea90746d106a99
Anti-code <Anti-code@users.noreply.github.com>
2016-04-04 00:44:58 +0300

    Now detects the ips

--- arping.py
+++ arping.py
@@ -1,19 +1,19 @@
 # coding: utf-8
 from scapy.all import *
 from collections import OrderedDict
-
+import subprocess
+import re
 class Devices(object):
-	def __init__(self, target_list = ["10.99.5."+str(x) for x in range(1,255)]):
+	def __init__(self):
+		ips = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.',subprocess.check_output(["ip","a"]).decode("ascii")) 
+		self.networks = list(set(x for x in ips if not x.startswith("127")))
+		self.target_list = (list(self.networks)[-1]+str(x) for x in range(1,255))
 		self.device_dict = OrderedDict()
-		self.target_list = target_list
 		self.scan()
-
 	def scan(self):
-		for ip in target_list:
-			print("tried-"+ip)
-			ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ip), timeout = 0.05)
-			for snd, rcv in ans:
-				self.device_dict[str(rcv.psrc)]=str(rcv.hwdst)
+		ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = self.target_list), timeout = 2)
+		for snd, rcv in ans:
+			self.device_dict[str(rcv.psrc)]=str(rcv.hwdst)
 
 if __name__ == "__main__":
 	d = Devices()
