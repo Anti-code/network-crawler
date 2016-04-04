40815c625e93f365b7223ae87fbf39c01e5f22fa
Anti-code <Anti-code@users.noreply.github.com>
2016-04-05 01:52:08 +0300

    Added files via upload

--- arping.py
+++ arping.py
@@ -8,15 +8,13 @@ class Devices(object):
 		ips = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.',subprocess.check_output(["ip","a"]).decode("ascii")) 
 		self.networks = list(set(x for x in ips if not x.startswith("127")))
 		self.target_list = (list(self.networks)[-1]+str(x) for x in range(1,255))
-		self.device_dict = OrderedDict()
-		self.scan()
 	def scan(self):
-		ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = self.target_list), timeout = 2)
+		device_dict = OrderedDict()
+		ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = self.target_list), timeout = 2.4)
 		for snd, rcv in ans:
-			self.device_dict[str(rcv.psrc)]=str(rcv.hwdst)
-
+			device_dict[str(rcv.psrc)]=str(rcv.hwdst)
+		return device_dict
 if __name__ == "__main__":
 	d = Devices()
-	for k,v in d.device_dict.items():
-		print("IP: ", k, "MAC", v)
+	
 
